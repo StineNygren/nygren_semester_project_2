b941de98fe92cf793000f23968b45c779797b762
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-11-15 16:09:31 +0100

    basic navigation set up

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const navigationSection = document.querySelector(".navigation-section");
+const toggleButton = document.querySelector(".toggle-butten");
+const navIcon = document.querySelector(".nav-icon");
+
+toggleButton.addEventListener("click", () => {
+  navigationSection.classList.toggle("hidden");
+  navIcon.classList.toggle("fa-x");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.mjs",
+    "./src/js/**/*.mjs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
