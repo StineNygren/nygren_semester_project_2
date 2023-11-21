2898ecb76f13fd9768f0a855f2d8d9bce97c62c0
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-11-21 15:34:04 +0100

    new version of navigation

--- src/js/layout/header.mjs
+++ src/js/layout/header.mjs
@@ -1,10 +1,15 @@
 const navigationSection = document.querySelector(".navigation-section");
 const toggleButton = document.querySelector(".toggle-butten");
 const navIcon = document.querySelector(".nav-icon");
+const auth_section = document.querySelector(".auth-section");
 
 function toggleNavigation() {
   toggleButton.addEventListener("click", () => {
     navigationSection.classList.toggle("hidden");
+    navigationSection.classList.toggle("flex");
+
+    auth_section.classList.toggle("hidden");
+    auth_section.classList.toggle("flex");
 
     navIcon.classList.toggle("fa-x");
   });
